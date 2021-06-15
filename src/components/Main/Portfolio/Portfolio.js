eddf5dd5bf993556cb4dc02e1e27491a186798be
Kris <kris_alexwa@mail.ru>
2021-06-15 18:37:15 +0500

    адаптив под мобильный, main полностью

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -10,19 +10,19 @@ function Portfolio() {
                     <h2 className="portfolio__title">Портфолио</h2>
                     <ul className="portfolio__list">
                         <li className="portfolio__list-item">
-                            <a href="#" target="_blank" className="portfolio__link">
+                            <a href="#" target="_blank" rel="noreferrer" className="portfolio__link">
                                 <p className="portfolio__link-title">Статичный сайт</p>
                                 <img className="portfolio__link-icon" src={ArrowIcon} alt="Иконка стрелка"></img>
                             </a>
                         </li>
                         <li className="portfolio__list-item">
-                            <a href="#" target="_blank" className="portfolio__link">
+                            <a href=" https://kris-alexwa.github.io/russian-travel/index.html" target="_blank" rel="noreferrer" className="portfolio__link">
                                 <p className="portfolio__link-title">Адаптивный сайт</p>
                                 <img className="portfolio__link-icon" src={ArrowIcon} alt="Иконка стрелка"></img>
                             </a>
                         </li>
                         <li className="portfolio__list-item">
-                            <a href="https://mesto-kris.nomoredomains.icu/sign-in" target="_blank" className="portfolio__link">
+                            <a href="https://mesto-kris.nomoredomains.icu/sign-in" target="_blank" rel="noreferrer" className="portfolio__link">
                                 <p className="portfolio__link-title">Одностраничное приложение</p>
                                 <img className="portfolio__link-icon" src={ArrowIcon} alt="Иконка стрелка"></img>
                             </a>
