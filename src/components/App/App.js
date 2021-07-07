8e3c0d0603187b66b201caa0020e49dbac40d408
Kris <kris_alexwa@mail.ru>
2021-07-08 00:26:06 +0300

    ошибки на странице логина и регистрации, обновление данных пользователя

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, useHistory } from 'react-router-dom';
+import { Route, Switch, Redirect, useHistory } from 'react-router-dom';
 import './App.css';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
@@ -7,13 +7,17 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import ProtectedRoute from '../ProtectedRoute';
 import * as userAuth from '../../utils/userAuth';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import { mainApi } from '../../utils/MainApi';
 // import ErrorPage from '../ErrorPage/ErrorPage';
 
 function App() {
-  // Отрисовка карточек на странице
   const [widthMode, setWidthMode] = React.useState(calcWidthMode());
-
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [currentUser, setCurrentUser] = React.useState({});
+  const token = localStorage.getItem('token');
 
   function calcWidthMode() {
     const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -37,35 +41,66 @@ function App() {
   const history = useHistory();
 
   function handleRegister(name, email, password) {
-    return userAuth.register(name, email, password).then(() => {
+    return userAuth.register(name, email, password)
+      .then(() => {
+        userAuth.authorize(email, password)
+          .then(() => {
+            setLoggedIn(true)
+            history.push('/movies')
+          })
+      })
+  }
+
+  function handleLogin(email, password) {
+    return userAuth.authorize(email, password).then((res) => {
+      setLoggedIn(true)
       history.push('/movies')
     })
   }
 
+  React.useEffect(() => {
+    if (!loggedIn) {
+      return;
+    }
+    mainApi.getUserInfo(token)
+      .then((res) => {
+        setCurrentUser({ name: res.name, email: res.email, id: res._id })
+      })
+      .catch((err) => {
+        alert('Не удалось загрузить информацию о пользователе')
+        console.log(err)
+      })
+  }, [loggedIn, token])
+
+  function signOut() {
+    localStorage.removeItem('token');
+    setLoggedIn(false)
+    history.push('/')
+  }
 
   return (
     <div className="body">
       <div className="page">
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route path="/signin">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Register onRegister={handleRegister} />
-          </Route>
-          <Route path="/movies">
-            <Movies widthMode={widthMode} />
-          </Route>
-          <Route path="/saved-movies">
-            <SavedMovies />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
+        <CurrentUserContext.Provider value={currentUser}>
+          <Switch>
+            <ProtectedRoute path="/movies" widthMode={widthMode} loggedIn={loggedIn} component={Movies}></ProtectedRoute>
+            <ProtectedRoute path="/saved-movies" loggedIn={loggedIn} component={SavedMovies}></ProtectedRoute>
+            <ProtectedRoute path="/profile" loggedIn={loggedIn} component={Profile} signOut={signOut} token={token} setCurrentUser={setCurrentUser}A>
+            </ProtectedRoute>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route path="/signin">
+              <Login onLogin={handleLogin} />
+            </Route>
+            <Route path="/signup">
+              <Register onRegister={handleRegister} userData={setCurrentUser} />
+            </Route>
+            <Route>
+              {loggedIn ? <Redirect to="/movies" /> : <Redirect to="/signin" />}
+            </Route>
+          </Switch>
+        </CurrentUserContext.Provider>
       </div>
     </div>
   );
